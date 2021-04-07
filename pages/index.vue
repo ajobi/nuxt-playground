03c8ee1799c486bab008d14bfac88dfaf50ff100
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-07 20:32:37 +0200

    New: Basic bubble chart setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,129 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-playground
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <canvas id="chartJSContainer" width="600" height="400" />
   </div>
 </template>
 
 <script>
-export default {}
+import { Chart } from 'chart.js/dist/Chart'
+
+export default {
+  mounted () {
+    const options = {
+      type: 'bubble',
+      data: {
+        datasets: [
+          {
+            data: [
+              {
+                id: 'point1',
+                x: 49,
+                y: 49,
+                r: 7
+              },
+              {
+                id: 'point2',
+                x: 52,
+                y: 47,
+                r: 7
+              },
+              {
+                id: 'point3',
+                x: 66,
+                y: 65,
+                r: 7
+              },
+              {
+                id: 'point4',
+                x: 69,
+                y: 69,
+                r: 7
+              },
+              {
+                id: 'point5',
+                x: 72,
+                y: 70,
+                r: 7
+              },
+              {
+                id: 'point6',
+                x: 46,
+                y: 45,
+                r: 7
+              }
+            ],
+            backgroundColor: '#88DBFD',
+            hoverBackgroundColor: '#88DBFD'
+          },
+          {
+            data: [
+              {
+                x: 50,
+                y: 50,
+                r: 100
+              },
+              {
+                x: 70,
+                y: 70,
+                r: 100
+              }
+            ],
+            borderColor: '#FBFDFF',
+            backgroundColor: '#FBFDFF',
+            hoverBackgroundColor: '#FBFDFF'
+          }
+        ]
+      },
+      options: {
+        tooltips: {
+          enabled: false
+        },
+        scales:
+          {
+            yAxes: [{
+              display: false,
+              ticks: {
+                min: 0,
+                max: 100
+              },
+              gridLines: {
+                display: false,
+                drawBorder: false
+              }
+            }],
+            xAxes: [{
+              display: false,
+              ticks: {
+                min: 0,
+                max: 100
+              },
+              gridLines: {
+                display: false,
+                drawBorder: false
+              }
+            }]
+          }
+      }
+    }
+
+    const canvas = document.getElementById('chartJSContainer')
+    const ctx = canvas.getContext('2d')
+
+    Chart.defaults.global.legend.display = false
+
+    // eslint-disable-next-line no-unused-vars
+    const myChart = new Chart(ctx, options)
+
+    canvas.onclick = function (e) {
+      const { _datasetIndex, _index } = myChart.getElementsAtEvent(e).filter(item => item._datasetIndex === 0)[0]
+      console.log(options.data.datasets[_datasetIndex].data[_index])
+    }
+  }
+}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background-color: hsl(208, 100%, 97%);
 }
 </style>
