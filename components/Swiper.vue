0f3774d45f61d29629834a5002bef75e22236825
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-19 16:41:47 +0100

    New: Transpiling external dependencies

--- components/Swiper.vue
+++ components/Swiper.vue
@@ -1,47 +1,47 @@
-<template>
-  <div>
-    Hello {{ count }}
-    <button @click="count++">
-      Increment
-    </button>
-  </div>
-</template>
-
-<script>
-import { ref } from '@vue/composition-api'
-
-export default {
-  setup () {
-    const count = ref(0)
-
-    return {
-      count
-    }
-  }
-}
-</script>
-
 <!--<template>-->
-<!--  <swiper-->
-<!--    :slides-per-view="3"-->
-<!--    :space-between="50"-->
-<!--  >-->
-<!--    <swiper-slide>Slide 1</swiper-slide>-->
-<!--    <swiper-slide>Slide 2</swiper-slide>-->
-<!--    <swiper-slide>Slide 3</swiper-slide>-->
-<!--  </swiper>-->
+<!--  <div>-->
+<!--    Hello {{ count }}-->
+<!--    <button @click="count++">-->
+<!--      Increment-->
+<!--    </button>-->
+<!--  </div>-->
 <!--</template>-->
-<!--<script>-->
-<!--// Import Swiper Vue.js components-->
-<!--import { Swiper, SwiperSlide } from 'swiper/vue'-->
 
-<!--// Import Swiper styles-->
-<!--import 'swiper/css'-->
+<!--<script>-->
+<!--import { ref } from '@vue/composition-api'-->
 
 <!--export default {-->
-<!--  components: {-->
-<!--    Swiper,-->
-<!--    SwiperSlide-->
+<!--  setup () {-->
+<!--    const count = ref(0)-->
+
+<!--    return {-->
+<!--      count-->
+<!--    }-->
 <!--  }-->
 <!--}-->
 <!--</script>-->
+
+<template>
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue/swiper-vue'
+
+// Import Swiper styles
+import 'swiper/swiper-bundle.css'
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,17 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [
+      /^swiper.*/,
+      /^ssr-window.*/,
+      /^vue-server-renderer.*/,
+      /^dom7.*/
+    ],
+    extend (config, { isClient }) {
+      // Extend only webpack config for client-bundle
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    }
   }
 }
