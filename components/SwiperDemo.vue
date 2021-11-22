73d8aaeb55a9c4599f9acdd62a632631029ba80b
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-22 10:27:06 +0100

    New: Demo cleanup

--- components/SwiperDemo.vue
+++ components/SwiperDemo.vue
@@ -29,6 +29,9 @@
     <swiper-slide>Slide 1</swiper-slide>
     <swiper-slide>Slide 2</swiper-slide>
     <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
   </swiper>
 </template>
 <script>
